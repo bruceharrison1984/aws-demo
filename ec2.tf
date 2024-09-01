bef7133c1f905968844f3073ef86057e36e22cdb
Bruce Harrison <leeharrison1984@gmail.com>
2024-08-31 23:36:02 -0500

    wip

--- ec2.tf
+++ ec2.tf
@@ -51,25 +51,25 @@ resource "aws_instance" "mongo" {
   }
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name   = "Allow SSH"
-  vpc_id = module.vpc.vpc_id
+# resource "aws_security_group" "allow_ssh" {
+#   name   = "Allow SSH"
+#   vpc_id = module.vpc.vpc_id
 
-  tags = {
-    Name = "Allow SSH"
-  }
-}
+#   tags = {
+#     Name = "Allow SSH"
+#   }
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+#   security_group_id = aws_security_group.allow_ssh.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 22
+#   ip_protocol       = "tcp"
+#   to_port           = 22
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.allow_ssh.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = -1
-}
+# resource "aws_vpc_security_group_egress_rule" "allow_ssh" {
+#   security_group_id = aws_security_group.allow_ssh.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = -1
+# }
