053d66bae8a9c05e73c096caed6b9868349dff29
Bruce Harrison <leeharrison1984@gmail.com>
2024-09-03 15:07:01 -0500

    wip

--- userdata/start.sh
+++ userdata/start.sh
@@ -3,15 +3,11 @@
 echo '- Get Admin Password'
 MONGO_ADMIN_PASS="$(grep -oP "\'.*\'" /home/bitnami/bitnami_credentials | awk '{ print $NF }' | sed "s/'//g")"
 
-echo '- Create Tasky DB'
-/home/bitnami/stack/mongodb/bin/mongosh -u root -p $MONGO_ADMIN_PASS --eval "use go-mongodb"
-echo '- Finished creating DB'
-
 echo 'Setting User Credentials'
 USERNAME=$(aws ssm get-parameter --name "${SSM_USERNAME_PATH}" --query "Parameter.Value" --output text --region us-east-1 --with-decryption)
 PASSWORD=$(aws ssm get-parameter --name "${SSM_PASSWORD_PATH}" --query "Parameter.Value" --output text --region us-east-1 --with-decryption)
 
-/home/bitnami/stack/mongodb/bin/mongosh -u root -p $MONGO_ADMIN_PASS --eval "db.createUser({ user: '$USERNAME', pwd: '$PASSWORD', roles: [{ role: 'readWrite', db: 'go-mongodb' }, { role: 'read', db: 'reporting' }] })"
+/home/bitnami/stack/mongodb/bin/mongosh -u root -p $MONGO_ADMIN_PASS --eval "use go-mongodb" --eval "db.createUser({ user: '$USERNAME', pwd: '$PASSWORD', roles: [{ role: 'readWrite', db: 'go-mongodb' }, { role: 'read', db: 'reporting' }] })"
 echo '- Finished setting credentials'
 
 echo '- Add S3 backup cron'
