bdbf6f7f99dfbf41b66b4d6b9f5c94d235ca754e
Bruce Harrison <leeharrison1984@gmail.com>
2024-09-02 23:32:29 -0500

    wip

--- modules/eks/terraform.tf
+++ modules/eks/terraform.tf
@@ -19,6 +19,7 @@ provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.default.token
+  load_config_file       = false
 }
 
 provider "helm" {
