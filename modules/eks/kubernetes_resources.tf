3ee528b7fbd5b74c4fbe873de0b609e5a5115234
Bruce Harrison <leeharrison1984@gmail.com>
2024-09-03 11:41:59 -0500

    update refs;

--- modules/eks/kubernetes_resources.tf
+++ modules/eks/kubernetes_resources.tf
@@ -83,7 +83,7 @@ YAML
 }
 
 resource "kubectl_manifest" "alb_controller_prereqs" {
-  for_each  = data.kubectl_file_documents.docs.manifests
+  for_each  = data.kubectl_file_documents.alb_controller_prereqs.manifests
   yaml_body = each.value
 }
 
@@ -190,6 +190,6 @@ YAML
 }
 
 resource "kubectl_manifest" "tasky_app" {
-  for_each  = data.kubectl_file_documents.docs.manifests
+  for_each  = data.kubectl_file_documents.tasky_app.manifests
   yaml_body = each.value
 }
